8a8878b1b29d6ab7deb55947218103fd4ad51b83
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-01-25 15:31:27 +0600

    spinner done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,13 +5,19 @@ const Header = () => {
     <div>
       <div className="navbar bg-base-300">
         <div className="navbar-start">
-            <a href="/" className="btn btn-ghost normal-case text-xl">Home</a>
+          <a href="/" className="btn btn-ghost normal-case text-xl">
+            Home
+          </a>
         </div>
         <div className="navbar-center">
-          <a href="/" className="btn btn-ghost normal-case text-xl">AWS Api Explore</a>
+          <a href="/" className="btn btn-ghost normal-case text-xl">
+            AWS Api Explore
+          </a>
         </div>
         <div className="navbar-end">
-            <a href="/" className="btn btn-ghost normal-case text-xl">About</a>
+          <a href="/" className="btn btn-ghost normal-case text-xl">
+            About
+          </a>
         </div>
       </div>
     </div>
